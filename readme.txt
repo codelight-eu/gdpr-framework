36ff4c389caab954e0b9682378f6f70fc9ec34c6
indrek_k <indrek@codelight.eu>
2018-05-17 19:47:38 +0000

    Bump version, fix merge conflict issues

--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: gdpr
 Requires at least: 4.7
 Tested up to: 4.9.5
 Requires PHP: 5.6.33
-Stable tag: 1.0.5
+Stable tag: 1.0.6
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
 
--- src/Updater/Updater.php
+++ src/Updater/Updater.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace Codelight\GDPR\Updater;
@@ -45,52 +44,4 @@ class Updater
 
         update_option('gdpr_plugin_version', '1.0.5');
     }
-=======
-<?php
-
-namespace Codelight\GDPR\Updater;
-
-class Updater
-{
-    public function __construct()
-    {
-        $currentVersion = $this->getVersion();
-
-        if ($currentVersion === GDPR_FRAMEWORK_VERSION) {
-            return;
-        }
-
-        if (version_compare($currentVersion, '1.0.5', '<')) {
-            $this->update_1_0_5();
-        }
-    }
-
-    /**
-     * @return string
-     */
-    public function getVersion()
-    {
-        return get_option('gdpr_plugin_version', '1.0.0');
-    }
-
-    /**
-     * Retroactively fix bug in v1.0.3 where the activation code didn't run properly
-     */
-    public function update_1_0_5()
-    {
-        $model = new \Codelight\GDPR\Components\Consent\UserConsentModel();
-        $model->createTable();
-
-        if (apply_filters('gdpr/data-subject/anonymize/change_role', true) && ! get_role('anonymous')) {
-
-            add_role(
-                'anonymous',
-                _x('Anonymous', '(Admin)', 'gdpr-framework'),
-                []
-            );
-        }
-
-        update_option('gdpr_plugin_version', '1.0.5');
-    }
->>>>>>> 66562f35d96cfd830396d9b54157e5900fffe364
 }
